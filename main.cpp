74a6d2da61b98086d3a793f8656e3f87c157be82
Arshia <ctuarshia@gmail.com>
2024-04-25 23:12:54 +0330

    added creat account to menu and changed exit code to 0

--- main.cpp
+++ main.cpp
@@ -9,15 +9,16 @@ using namespace std;
 
 int main() {
 menu:
-    cout << "Login as master or student(press 1 or 2 for login and 3 for exit)\n "
+    cout << "Login as master or student or creat account (press 1 or 2 or 3 for login and 0 for exit)\n"
+            " 0. exit\n "
             "1. Student\n "
             "2. Master\n "
-            "3. exit\n";
+            "3. Register\n";
     int op = 0;
     cin >> op;
     if(op == 1){
-        cout << "Please enter your username and password: ";
-        string input;
+        cout << "Please enter your username and password:";
+        string input = "";
         getline(cin >> ws, input);
         fstream studentfile;
         studentfile.open("StudentsDB.txt", ios::in);//read from student file
@@ -37,8 +38,8 @@ menu:
 
     }
     if(op == 2){
-        cout << "Please enter your username and password: ";
-        string input;
+        cout << "Please enter your username and password:";
+        string input = "";
         getline(cin >> ws, input);
         fstream masterfile;
         masterfile.open("MastersDB.txt", ios::in);//read from master file
@@ -58,6 +59,37 @@ menu:
 
     }
     if(op == 3){
+        string op2;
+        cout << "Are you master or student? ";
+        cin >> op2;
+        if(op2 == "master"){
+            cout << "Please enter a username and a password:";
+            string input = "";
+            getline(cin >> ws, input);
+            fstream mastercrfile;
+            mastercrfile.open("MastersDB.txt", ios::app); //append to master file
+            if(mastercrfile.is_open()){
+                mastercrfile << endl << input;
+                cout << "\nYou have successfully created an account.\nlogging in...\n";
+                mastercrfile.close();
+            }
+            cout << "You have successfully logged in as master\n";
+        }
+        else if(op2 == "student"){
+            cout << "Please enter a username and a password: ";
+            string input = "";
+            getline(cin >> ws, input);
+            fstream studentcrfile;
+            studentcrfile.open("StudentsDB.txt", ios::app); //append to student file
+            if(studentcrfile.is_open()){
+                studentcrfile << endl << input;
+                cout << "\nYou have successfully created an account.\nlogging in...\n";
+                studentcrfile.close();
+            }
+            cout << "You have successfully logged in as student\n";
+        }
+    }
+    if(op == 0){
         exit(0);
     }
 
